2da7bac806333994099fb8b9ecb268c737210de9
ulempicka <urszula.lempicka@gmail.com>
2020-07-30 01:53:30 +0200

    Commit4 - Cards in progress

--- src/cards/SetPerColor.java
+++ src/cards/SetPerColor.java
@@ -1,10 +1,8 @@
 package cards;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-public enum SetPerColor {
-    SET(new int[]{2, 3, 4, 5, 6, 7, 8, 9, 10}, new String[]{"walet", "dama", "król", "as"});
+public class SetPerColor {
 
     private final int[] numbers;
     private final String[] figures;
@@ -12,5 +10,37 @@ public enum SetPerColor {
     SetPerColor(int[] numbers, String[] figures) {
         this.numbers = numbers;
         this.figures = figures;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public String[] getFigures() {
+        return figures;
+    }
+
+    @Override
+    public String toString() {
+        return "SetPerColor{" +
+                "numbers=" + Arrays.toString(numbers) +
+                ", figures=" + Arrays.toString(figures) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SetPerColor that = (SetPerColor) o;
+        return Arrays.equals(numbers, that.numbers) &&
+                Arrays.equals(figures, that.figures);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(numbers);
+        result = 31 * result + Arrays.hashCode(figures);
+        return result;
     }
 }
